3b9b10a3dba03d13be3d569b14e437bab6d75431
gutodallacb <gutodallabomba@gmail.com>
2026-01-31 09:59:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-document.querySelectorAll(".servico").forEach((servico) => {
-  servico.querySelector(".servico-header").addEventListener("click", () => {
+document.querySelectorAll(".toggle").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const servico = btn.closest(".servico");
+
+    document.querySelectorAll(".servico").forEach((s) => {
+      if (s !== servico) s.classList.remove("active");
+    });
+
     servico.classList.toggle("active");
+  });
+});
+document.querySelectorAll(".faq-item").forEach((item) => {
+  const btn = item.querySelector(".faq-question");
+
+  btn.addEventListener("click", () => {
+    const isOpen = item.classList.contains("active");
+
+    // fecha todos
+    document.querySelectorAll(".faq-item").forEach((i) => {
+      i.classList.remove("active");
+    });
+
+    // se não estava aberto, abre
+    if (!isOpen) item.classList.add("active");
   });
 });
